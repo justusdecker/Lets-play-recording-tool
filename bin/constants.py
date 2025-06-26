5653918e08bc76812f599492e72800a129ccf9e0
Justus <justus.d2025@gmail.com>
2025-06-26 13:29:29 +0200

    0.1.27

--- bin/constants.py
+++ bin/constants.py
@@ -4,8 +4,8 @@ MENU_MESSAGE = f"""
 {bold('LPRT')} - (c) Justus Decker 2024 - 2025
 
 Select your option:
-(1) Record
-(2) Automation
+(1) Record - {color816(bold('ALPHA'),35)}
+(2) {color816(strikethrough('Automation'),31)}
 (3) {color816(strikethrough('Deploy'),31)}
 (4) {color816(strikethrough('Distribute'),fg=31)}
 """
