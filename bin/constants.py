db7bcb2196b0b05b0cad4001c5b000df7fba07a1
Justus <justus.d2025@gmail.com>
2025-06-26 13:34:41 +0200

    0.1.30

--- bin/constants.py
+++ bin/constants.py
@@ -2,7 +2,7 @@ from bin.text_manipulation import *
 from bin.data_access import file_read
 
 MENU_MESSAGE = f"""
-{bold('LPRT')} Version: {italic(file_read('version.txt'))} - (c) Justus Decker 2024 - 2025
+{bold('LPRT')} {italic(file_read('version.txt'))} - (c) Justus Decker 2024 - 2025
 
 Select your option:
 (1) Record - {color816(bold('ALPHA'),35)}
