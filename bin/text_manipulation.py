dbbfba44ae187ee088560aa24023a2c0277c9528
Justus <justus.d2025@gmail.com>
2025-06-26 13:06:28 +0200

    0.1.15

--- bin/text_manipulation.py
+++ bin/text_manipulation.py
@@ -2,7 +2,7 @@ COLOR_TABLE816 = [30 + i for i in range(10)] + [40 + i for i in range(10)] + [90
 print(COLOR_TABLE816)
 def strikethrough(text: str) -> str:
     """ set strikethrough mode by using escape sequences """
-    return f"\033[9m{text}\033[9m"
+    return f"\033[9m{text}\033[29m"
 def color816(text: str, fg: int, bg: int) -> str:
     """ 
     set text color by using escape sequences.
