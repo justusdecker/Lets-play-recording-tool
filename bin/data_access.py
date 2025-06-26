3a6c99fd0d3405038f951957b3e40a7b3cac8ae2
Justus <justus.d2025@gmail.com>
2025-06-26 14:26:37 +0200

    0.1.35

--- bin/data_access.py
+++ bin/data_access.py
@@ -1,3 +1,9 @@
+import json
+
 def file_read(filepath : str) -> str:
     with open(filepath, 'r') as f:
         return f.read()
+
+def json_read(filepath : str) -> dict | list:
+    with open(filepath, 'r') as f:
+        return json.loads(f)
