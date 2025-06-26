e23b97b4d25b9a96b8c7c1bea87d2fe5e69a3a19
Justus <justus.d2025@gmail.com>
2025-06-26 12:58:00 +0200

    0.1.12

--- bin/text_manipulation.py
+++ bin/text_manipulation.py
@@ -8,14 +8,22 @@ def tcolor(text: str, color: int) -> str:
     |Color|FG CC|BG CC|
     |---|---|---|
     |Black|30|40|
-    Red	31	41
-    Green	32	42
-    Yellow	33	43
-    Blue	34	44
-    Magenta	35	45
-    Cyan	36	46
-    White	37	47
-    Default	39	49
+    |Red|31|41|
+    |Green|32|42|
+    |Yellow|33|43|
+    |Blue|34|44|
+    |Magenta|35|45|
+    |Cyan|36|46|
+    |White|37|47|
+    |Default|39|49|
+    Bright Black	90	100|
+    Bright Red	91	101|
+    Bright Green	92	102|
+    Bright Yellow	93	103|
+    Bright Blue	94	104|
+    Bright Magenta	95	105|
+    Bright Cyan	96	106|
+    Bright White	97	107|
     
     """
     return f"\033[9m{text}\033[9m"
