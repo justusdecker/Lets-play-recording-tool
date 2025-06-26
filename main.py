56ca62d57a304731d9c73ac8d408416a1db802cd
Justus <justus.d2025@gmail.com>
2025-06-26 22:06:20 +0200

    0.1.47

--- main.py
+++ main.py
@@ -3,12 +3,8 @@ from bin.constants import MENU_MESSAGE, USER_INPUT_NUM_UNMATCHED, MENU_AUTOMATIO
 from bin.obs import OBSObserver
 class App:
     def __init__(self):
-        self.user_input = ""
-        self.args_input = ""
         self.isrunning = True
-    @property
-    def args(self) -> list[str]:
-        return self.user_input.split(' ')
+        
     def main_menu(self):
         match int(self.user_input):
             case 1:
@@ -22,19 +18,21 @@ class App:
                 self.isrunning = False
             case _:
                 print(USER_INPUT_NUM_UNMATCHED)
+                
     def automation_sub_menu(self):
         while self.isrunning:
             print(MENU_AUTOMATION_MESSAGE)
-            self.user_input = input()
-            if not self.user_input.isdecimal():
+            user_input = input()
+            if not user_input.isdecimal():
                 continue
+        
     def loop(self):
         while self.isrunning:
             print(MENU_MESSAGE)
-            self.user_input = input()
-            if not self.user_input.isdecimal():
+            user_input = input()
+            if not user_input.isdecimal():
                 continue
-            match int(self.user_input):
+            match int(user_input):
                 case 1:
                     OBSO = OBSObserver()
                     print(OBSO.timecode)
