43c65caaadf23c4aa43938ff6c221bd2e38a522f
Justus <justus.d2025@gmail.com>
2025-06-26 22:44:46 +0200

    0.1.53

--- bin/data_access.py
+++ bin/data_access.py
@@ -1,4 +1,11 @@
 import json
+import csv
+
+def csv_read(filepath : str):
+    with open(filepath) as csvfile:
+        reader = csv.reader(csvfile)
+        return [row for row in reader]
+            
 
 def file_read(filepath : str) -> str:
     with open(filepath, 'r') as f:
