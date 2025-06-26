847c724b977e7c567978619f88e80dee5ba30654
Justus <justus.d2025@gmail.com>
2025-06-26 12:57:09 +0200

    0.1.11

--- bin/text_manipulation.py
+++ bin/text_manipulation.py
@@ -1,3 +1,21 @@
 def strikethrough(text: str) -> str:
     """ set strikethrough mode by using escape sequences """
     return f"\033[9m{text}\033[9m"
+def tcolor(text: str, color: int) -> str:
+    """ 
+    set text color by using escape sequences.
+    
+    |Color|FG CC|BG CC|
+    |---|---|---|
+    |Black|30|40|
+    Red	31	41
+    Green	32	42
+    Yellow	33	43
+    Blue	34	44
+    Magenta	35	45
+    Cyan	36	46
+    White	37	47
+    Default	39	49
+    
+    """
+    return f"\033[9m{text}\033[9m"
