7ef1a5b63974e1adaee922d31b7c34d915d6a465
Justus <justus.d2025@gmail.com>
2025-06-27 14:09:26 +0200

    0.1.93

--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+from bin.data_access import file_write, file_append
 class Tests:
     
     def __init__(self,meat):
@@ -6,3 +7,11 @@ class Tests:
         
     def add(self,function_title: str):
         self.ok_tests.append(function_title)
+        
+    def write(self):
+        file_write('testing.md','')
+        for test in self.all_tests:
+            if test in self.ok_tests:
+                file_append('testing.md',f'> SUCCESS\n> {test}')
+            else:
+                file_append('testing.md',f'> [!CAUTION]\n> {test}')
