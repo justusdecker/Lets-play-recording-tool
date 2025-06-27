5657576ba5a012dac023ee918d9d9e4e80d12374
Justus <justus.d2025@gmail.com>
2025-06-27 13:50:59 +0200

    0.1.85

--- tests.py
+++ tests.py
@@ -1,11 +1,8 @@
-import testing.test_data_access, testing.test_obs
-
 class Tests:
-    def __init__(self):
-        meat = dir(testing.test_data_access) + dir(testing.test_obs)
-        self.all_tests = [i for i in meat if i.startswith('test')]
+    
+    def __init__(self,meat: list):
+        self.all_tests = [i for i in dir(meat) if i.startswith('test')]
         self.ok_tests = []
+        
     def add(self,function_title: str):
         self.ok_tests.append(function_title)
-
-print(dir(testing.test_data_access))
