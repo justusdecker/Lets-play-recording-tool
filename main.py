390b9a4f0eea7f30e6385e1dedc792c4c86647ea
Justus <justus.d2025@gmail.com>
2025-06-27 15:09:46 +0200

    0.1.100

--- main.py
+++ main.py
@@ -14,18 +14,30 @@ class App:
             case 1:
                 OBSO = OBSObserver()
                 print(OBSO.timecode)
-                
-                
                 while OBSO.isconnected:
                     print(OBSO.timecode)
                     
             case 2:
                 self.automation_sub_menu()
             case 5:
+                pass
+            case 0:
                 self.isrunning = False
             case _:
                 print(USER_INPUT_NUM_UNMATCHED)
-                
+    def data_sub_menu(self):
+        while self.isrunning:
+            print(MENU_DATA_MESSAGE)
+            user_input = input()
+            if not user_input.isdecimal():
+                continue
+            match int(user_input):
+                case 1:
+                    pass
+                case 5:
+                    return
+                case _:
+                    print(USER_INPUT_NUM_UNMATCHED)
     def automation_sub_menu(self):
         while self.isrunning:
             print(MENU_AUTOMATION_MESSAGE)
