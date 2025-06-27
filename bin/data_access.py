d9d30199bfb43448507242109971ccec724724d1
Justus <justus.d2025@gmail.com>
2025-06-27 03:37:43 +0200

    0.1.54

--- bin/data_access.py
+++ bin/data_access.py
@@ -1,11 +1,15 @@
 import json
 import csv
 
-def csv_read(filepath : str):
-    with open(filepath) as csvfile:
-        reader = csv.reader(csvfile)
+def csv_read(filepath : str) -> list[list[str]]:
+    with open(filepath,newline='') as f:
+        reader = csv.reader(f, delimiter='|')
         return [row for row in reader]
-            
+
+def csv_write(filepath : str,data : list) -> None:
+    with open(filepath,'w',newline='') as f:
+        writer = csv.writer(f, delimiter='|')
+        writer.writerows(data)
 
 def file_read(filepath : str) -> str:
     with open(filepath, 'r') as f:
@@ -14,3 +18,5 @@ def file_read(filepath : str) -> str:
 def json_read(filepath : str) -> dict | list:
     with open(filepath, 'r') as f:
         return json.load(f)
+    
+csv_write('test.csv',[[1,41,341,43],[1,41,341,43]])
