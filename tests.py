05bb4dec30811c4422f2bb93bea5abc2145115bb
Justus <justus.d2025@gmail.com>
2025-06-27 14:05:49 +0200

    0.1.90

--- tests.py
+++ tests.py
@@ -1,18 +1,8 @@
 class Tests:
     
-    def __init__(self,meat: list):
+    def __init__(self,meat):
         self.all_tests = [i for i in dir(meat) if i.startswith('test')]
         self.ok_tests = []
         
     def add(self,function_title: str):
         self.ok_tests.append(function_title)
-
-
-def test_T():pass
-def test_3(): pass
-def lol(): pass
-test = test_T()
-
-TESTS = Tests(__all__)
-print(__spec__)
-print(TESTS.all_tests)
