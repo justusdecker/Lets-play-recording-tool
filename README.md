44fd2e706486001d3d85e9cce3048c08dde91a76
Justus <justus.d2025@gmail.com>
2025-06-28 21:29:03 +0200

    0.2.40

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 
 # Lets-play-recording-tool
-## 0.2.39
+## 0.2.40
 
 An automation tool for lets players
 
--- bin/others.py
+++ bin/others.py
@@ -26,3 +26,7 @@ def binps(text : str, inp: str = '') -> int:
         print('\033[23m\033[39m',end='')
     return user_input
     
+def input_episode_range(max_lps: int, max_eps:list[int], lp_names: list[str]):
+    listed_lets_plays = "\n".join([f"({idx}) {lp}" for idx, lp in enumerate(lp_names)])
+    binpi(f'Select Lets Play ID: \n {listed_lets_plays}')
+    
