33ed46fb8afba710407fc6f71b0d29b1ffaf3326
Justus <justus.d2025@gmail.com>
2025-06-28 17:42:06 +0200

    0.2.38

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 
 # Lets-play-recording-tool
-## 0.2.37
+## 0.2.38
 
 An automation tool for lets players
 
--- testing/test_thumbnail_gen.py
+++ testing/test_thumbnail_gen.py
@@ -5,3 +5,6 @@ def test_create_tg():
 def test_create_thumbnail():
     TG = ThumbnailGenerator()
     TG.generate('123','test.mp4','tad_test.json','test.png',0)
+def test_create_thumbnail_random_image():
+    TG = ThumbnailGenerator()
+    TG.generate('123','test.mp4','tad_test.json','test.png',-1)
