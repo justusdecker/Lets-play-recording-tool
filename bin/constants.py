f5fe58ca532ebb7cda04a7bb8419b9b17da11dcd
Justus <justus.d2025@gmail.com>
2025-06-29 05:11:10 +0200

    0.3.7

--- bin/constants.py
+++ bin/constants.py
@@ -32,7 +32,7 @@ MENU_MESSAGE = f"""
 MENU_AUTOMATION_MESSAGE = f"""
 {header('automations')}
 (1) Thumbnail Generate
-(2) {color816(strikethrough('Fetch Audio'),31)}
+(2) Fetch Audio
 (3) {color816(strikethrough('Fix Audio'),31)}
 (4) {color816(strikethrough('Render'),31)}
 (0) Back
