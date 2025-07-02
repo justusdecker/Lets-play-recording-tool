cd1184a466b22a22f3e48248eb434c0147272f66
Justus <justus.d2025@gmail.com>
2025-07-02 03:13:18 +0200

    0.3.111

--- bin/resolve_sender.py
+++ bin/resolve_sender.py
@@ -1,4 +1,12 @@
-""""""
+__author__ = "Justus Decker"
+__copyright__ = "(c) 2024 - 2025 , The LPRT Project"
+__credits__ = []
+__license__ = "CC BY-NC-ND"
+__version__ = "0.3.111"
+__maintainer__ = "Justus Decker"
+__email__ = "justus.d2025@gmail.com"
+__status__ = "Testing"
+
 from os.path import isfile
 from bin.data_access import file_read, file_write
 from time import time
