17b857b80d16a88efc7b4babc9a5409530a00409
Justus <justus.d2025@gmail.com>
2025-07-02 03:15:21 +0200

    0.3.117

--- bin/audio.py
+++ bin/audio.py
@@ -1,3 +1,12 @@
+__author__ = "Justus Decker"
+__copyright__ = "(c) 2024 - 2025 , The LPRT Project"
+__credits__ = []
+__license__ = "CC BY-NC-ND"
+__version__ = "0.3.117"
+__maintainer__ = "Justus Decker"
+__email__ = "justus.d2025@gmail.com"
+__status__ = "Testing"
+
 from os.path import isfile
 from moviepy.audio.io.AudioFileClip import AudioFileClip
 import subprocess
