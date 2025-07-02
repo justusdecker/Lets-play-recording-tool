67caf348e74d49483226e160b6540625ffa9ee73
Justus <justus.d2025@gmail.com>
2025-07-02 13:47:31 +0200

    0.3.119

---
+++ bin/audio_player.py
@@ -0,0 +1,30 @@
+#from pydub import AudioSegment
+
+# Pydub is broken
+# playsound
+# wave
+#pyaudio noplayback
+
+import pygame as pg
+
+class AudioPlayer:
+    def __init__(self,t1,t2):
+        self.isrunning = True
+        self.display = pg.display.set_mode((300,200))
+    def run(self):
+        self.update()
+    def update(self):
+        while self.isrunning:
+            
+            # Back button
+            # Foreward button
+            # play button
+            #
+            
+            pg.draw.rect(self.display,(25,25,25),(0,100,100,100))
+            
+            for e in pg.event.get():
+                if e.type == pg.QUIT:
+                    self.isrunning = False
+AP = AudioPlayer('','')
+AP.run()
