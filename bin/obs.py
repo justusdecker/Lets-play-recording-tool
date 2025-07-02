51f7ce5cc3eb99e3fefc8aa816444bff90866cec
Justus <justus.d2025@gmail.com>
2025-07-02 03:07:12 +0200

    0.3.106

--- bin/obs.py
+++ bin/obs.py
@@ -1,15 +1,29 @@
+__author__ = "Justus Decker"
+__copyright__ = "(c) 2024 - 2025 , The LPRT Project"
+__credits__ = ["Adem Atikturk"]
+__license__ = "CC BY-NC-ND" # No changes in the obs_ws source code!
+__version__ = "0.3.106"
+__maintainer__ = "Justus Decker"
+__email__ = "justus.d2025@gmail.com"
+__status__ = "Production"
+
 import obsws_python as obsws
 from websocket import _exceptions
 from bin.data_access import json_read
 
 OUTPUT_TYPE = 'adv_file_output'
 class OBSObserver:
+    """
+    A wrapper for the obs_ws API
+    """
     def __init__(self):
         self.settings = json_read('settings.json')
         self.connect()
         self.recording_flag = False #used for one_time operations like on_start
-    
     def update(self,ep):
+        """
+        Call this method to check on_start & on_stop events
+        """
         self.connect() # Reconnect
         if self.isrecording and not self.recording_flag: # Recording started
             self.recording_flag = True
@@ -18,6 +32,11 @@ class OBSObserver:
         elif not self.isrecording and self.recording_flag: # Recording stopped
             self.recording_flag = False
     def connect(self):
+        """
+        connects to the API
+        
+        Needs the `settings.json`
+        """
         if self.isconnected:
            return 
         try:
@@ -26,9 +45,11 @@ class OBSObserver:
             print('WindowsError')
         except _exceptions.WebSocketTimeoutException as E:
             print('WebsocketTimeout')
-    
     @property
     def isconnected(self) -> bool:
+        """
+        Get the connection state
+        """
         if hasattr(self,'client'):
             try:
                 self.client.get_stats()
@@ -38,19 +59,25 @@ class OBSObserver:
         return False
     @property
     def isrecording(self) -> bool:
+        """
+        Get the recording state
+        """
         return self.timecode != '00:00:00.000'
-    
     @property
     def filepath(self) -> str:
+        """
+        Gets the current filepath from the obs_ws API
+        """
         return str(self.client.get_output_settings(OUTPUT_TYPE).output_settings['path'])
-    
     @property
     def time_in_seconds(self) -> int:
+        """
+        Get the recording time in seconds
+        """
         hms, _ = self.timecode.split('.')
         h,m,s = hms.split(':')
         h, m = int(h) * 3600, int(m) * 60 
         return h + m + int(s)
-    
     @property
     def timecode(self) -> str:
         """Get The Current Time In String Form"""
