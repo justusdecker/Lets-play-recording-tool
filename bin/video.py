5f802cc7e4a758b69472045a8b865b65adabd19d
Justus <justus.d2025@gmail.com>
2025-07-03 04:28:13 +0200

    0.4.7

---
+++ bin/video.py
@@ -0,0 +1,27 @@
+import subprocess
+
+def render(vid,aud):
+    """        
+    """
+    
+    subprocess.run(
+                (
+                    'ffmpeg',
+                    '-y',               # Will replace existing output
+                    '-i',               # Input filepath 1
+                    '-an',
+                    f"{vid}",            # Input filepath 1
+                    
+
+                    '-i',               # Input filepath 2
+                    f"{aud}",            # Input filepath 2
+
+                    
+                    '-filter_complex',  #for merging
+                    'amerge=inputs=2',  # For merging
+                    '-ac', '2',         # Set audio channel
+                    f"temp.mp3"         # output filepath
+                    ),
+                subprocess.CREATE_NO_WINDOW,
+                shell= True
+                )
