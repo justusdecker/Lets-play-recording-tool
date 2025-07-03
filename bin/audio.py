6058988d4de2b0a97f7f280e2d30a8a5973388e4
Justus <justus.d2025@gmail.com>
2025-07-03 04:29:25 +0200

    0.4.10

--- bin/audio.py
+++ bin/audio.py
@@ -24,10 +24,6 @@ def combine_audio(t1: str,t2: str, vol: float):
         The mic track
     .. t2::
         The desktop track (This track will be volume modified)
-    .. st::
-        starttime in HH:MM:SS format must be a `str`
-    .. et::
-        endtime in HH:MM:SS format must be a `str`
     
     """
     #ffmpeg -ss 00:01:00 -to 00:02:00 -i {input_file} -c copy {result_file}
