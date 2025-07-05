1c6f561138f11420edf53f8c6c2846b9b9c27c78
Justus <justus.d2025@gmail.com>
2025-07-05 04:24:43 +0200

    0.4.48

--- testing.py
+++ testing.py
@@ -1,5 +1,7 @@
 import pytest
 from bin.constants import ffmpeg_run
-from bin.constants import FFMPEG_EXTRACT
+from bin.constants import FFMPEG_EXTRACT, FFMPEG_LOUDNESS_NORMALIZATION, FFMPEG_LIMITER
 
-ffmpeg_run(FFMPEG_EXTRACT,{'__IN__': 'C:/Users/Justus/Videos/2025-06-29 22-43-39.mp4','__OUT__':'test.mp3','__MAPPING__':'1'})
+ffmpeg_run(FFMPEG_LOUDNESS_NORMALIZATION,{'__IN__': 'test.mp3','__OUT__':'test_1.mp3'})
+ffmpeg_run(FFMPEG_LIMITER,{'__IN__': 'test_1.mp3','__OUT__':'test_2.mp3'})
+#ffmpeg_run(FFMPEG_EXTRACT,{'__IN__': 'C:/Users/Justus/Videos/2025-06-29 22-43-39.mp4','__OUT__':'test.mp3','__MAPPING__':'1'})
