60a378f9f51b5bb99b09f5596293637b083a0964
Justus <justus.d2025@gmail.com>
2025-07-05 06:15:08 +0200

    0.4.80

--- bin/automations.py
+++ bin/automations.py
@@ -241,7 +241,7 @@ def compare_audio():
             AP.run()
             volume = AP.vol
             del AP
-            ffmpeg_run(FFMPEG_VOLUME_APPLIER,{'__IN__':desk,'__VOLUME__': volume})
+            ffmpeg_run(FFMPEG_VOLUME_APPLIER,{'__IN__':desk,'__VOLUME__': str(volume)})
             ffmpeg_run(FFMPEG_AUDIO_COMBINE,{'__IN__':mic,'__OUT__':final_path})
             print(mic, desk, volume)
             #ep.set_final_audio_path(epr[0],final_path)
@@ -256,7 +256,7 @@ def compare_audio():
                 AP.run()
                 volume = AP.vol
                 del AP
-                ffmpeg_run(FFMPEG_VOLUME_APPLIER,{'__IN__':desk,'__VOLUME__': volume})
+                ffmpeg_run(FFMPEG_VOLUME_APPLIER,{'__IN__':desk,'__VOLUME__': str(volume)})
                 ffmpeg_run(FFMPEG_AUDIO_COMBINE,{'__IN__':mic,'__OUT__':final_path})
                 print(mic, desk, volume)
                 #ep.set_final_audio_path(i,final_path)
