1010f66badf87e94b18f4b71439c15f95b9db391
Justus <justus.d2025@gmail.com>
2025-07-08 14:09:24 +0200

    0.4.100

--- bin/automations.py
+++ bin/automations.py
@@ -231,46 +231,33 @@ def compare_audio_and_render():
         lp,epr = res
         ep_path = letsplay.get_episode_path(lp)
         ep = Episode(ep_path)
-        if epr[0] == epr[1]:
-            final_path = f'{AUDIO_FOLDER}{epr[0]+1}_{letsplay.get_game_name(lp)}_final.mp3'
-            mic = ep.get_audio_mic_path(epr[0])
-            desk = ep.get_audio_desktop_path(epr[0])
-            
+        
+
+        # Only this part must be edited
+        # The part above is abandoned. A much simpler approch like relink epr to use in a list will work i think
+        # This count for every function that does use this workflow
+        for i in range(epr[0],epr[1]):
+            print(i)
+        print(epr)
+        if epr[0] == epr[1]:pass
+        pass
+        rendering_queue = []
+        for i in range(epr[0],epr[1]):
+            final_path = f'{AUDIO_FOLDER}{i+1}_{letsplay.get_game_name(lp)}_final.mp3'
+            mic = ep.get_audio_mic_path(i)
+            desk = ep.get_audio_desktop_path(i)
+            inf(f'{mic} {desk}')
             AP = AudioPlayer(mic, desk)
             AP.run()
             volume = AP.vol
             del AP
-            ffmpeg_run(FFMPEG_AUDIO_COMBINE,{'__IN1__':mic,'__IN2__': desk,'__VOLUME1__': str(1.0),'__VOLUME2__': str(volume),'__OUT__':'temp_audio_final.mp3'})
+            ffmpeg_run(FFMPEG_AUDIO_COMBINE,{'__IN1__':mic,'__IN2__': desk,'__VOLUME1__': str(1.0),'__VOLUME2__': str(volume),'__OUT__':f'temp{i+1}_audio_final.mp3'})
             print(mic, desk, volume)
             
+            
             #! NO
-            ep.set_final_audio_path(epr[0],final_path)
+            ep.set_final_audio_path(i,final_path)
             ep.save()
-        else:
-            # Only this part must be edited
-            # The part above is abandoned. A much simpler approch like relink epr to use in a list will work i think
-            # This count for every function that does use this workflow
-            for i in range(epr[0],epr[1]):
-                print(i)
-            print(epr)
-            pass
-            rendering_queue = []
-            for i in range(epr[0],epr[1]):
-                final_path = f'{AUDIO_FOLDER}{i+1}_{letsplay.get_game_name(lp)}_final.mp3'
-                mic = ep.get_audio_mic_path(i)
-                desk = ep.get_audio_desktop_path(i)
-                inf(f'{mic} {desk}')
-                AP = AudioPlayer(mic, desk)
-                AP.run()
-                volume = AP.vol
-                del AP
-                ffmpeg_run(FFMPEG_AUDIO_COMBINE,{'__IN1__':mic,'__IN2__': desk,'__VOLUME1__': str(1.0),'__VOLUME2__': str(volume),'__OUT__':f'temp{i+1}_audio_final.mp3'})
-                print(mic, desk, volume)
-                
-                
-                #! NO
-                ep.set_final_audio_path(i,final_path)
-                ep.save()
  
 def deploy(lp: LetsPlay, ep: Episode,id: int):
     """
