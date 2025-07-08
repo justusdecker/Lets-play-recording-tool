1bc6e6bfdf8928051f1cc093e811ca573805944e
Justus <justus.d2025@gmail.com>
2025-07-08 22:47:18 +0200

    0.5.15

--- bin/constants.py
+++ bin/constants.py
@@ -13,22 +13,17 @@ from bin.version import VERSION
 
 COPYRIGHT = f"{bold('LPRT')} {italic(VERSION)} - (c) Justus Decker 2024 - 2025"
 
-SUBS = {
-    'main': ['Main'],
-    'automations': ['Main','Automations'],
-    'options': ['Main','Options'],
-    'tg': ['Main' , 'Automation' , 'Thumbnail Generator']
-}
-
-def header(key_sub: str,subs: list[str]) -> str:
-    tmp = bold(" > ").join(SUBS[key_sub]+subs) + ' >'
-    return f'{COPYRIGHT}\n\n{tmp}\n\nSelect your option:'
+SUBS = { 'main': ['Main'],'automations': ['Main','Automations'],'options': ['Main','Options']}
 
 MENU_OPTIONS = ['Record', 'Automation', color816(strikethrough('Deploy'),31), 'Options'] # main
 
 MENU_AUTOMATION_OPTIONS = ['Thumbnail Generate', 'Fetch Audio', 'Fix Audio', 'Compare Audio & render'] # automations
 
 MENU_SETTINGS_OPTIONS = [f"Create {bold('options.json')} - OBS", 'Set current lets play id', f"Create {bold('default_tad.json')}", f"Create {bold('lets_plays.csv')}"] # options
+
+def header(key_sub: str,subs: list[str]) -> str:
+    tmp = bold(" > ").join(SUBS[key_sub]+subs) + ' >'
+    return f'{COPYRIGHT}\n\n{tmp}\n\nSelect your option:'
 
 def menu(options: list[str], key: str, subs: list[str] = [], exit_name: str = 'Exit'):
     """
