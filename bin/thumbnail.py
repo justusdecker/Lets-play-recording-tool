179476956e016a9f11b9cc0f15103eb3bfbfb457
Justus <justus.d2025@gmail.com>
2025-07-08 17:59:54 +0200

    0.5.5

--- bin/thumbnail.py
+++ bin/thumbnail.py
@@ -170,7 +170,7 @@ class ThumbnailGenerator:
         mpx = tad['pos'][0] + rx
         mpy = tad['pos'][1] + ry
         img = self.__get_src_image(filepath, frame)
-        
+        print(mpx,mpy)
         a, b = tad['r_scale']
         a, b = int(a * 100), int(b * 100)
         s = tad['scale'] + (ri(a, b) / 100)
