10da7042e0c7734a09ee2d0554c08ba30a47b6ae
Justus <justus.d2025@gmail.com>
2025-07-09 02:00:44 +0200

    0.7.6

--- testing/test_markdown_gen.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from bin.data_access import *
-from bin.automations import generate_markdown
-def test_gen():
-    lp = LetsPlay('lets_plays.csv')
-    
-    ep = Episode('eps_schedule_one.csv')
-    
-    generate_markdown(lp,ep,0)
--- testing/test_obs.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from bin.obs import OBSObserver
-
-def test_connection():
-    """
-    Make sure you have OBS open!
-    """
-    OBSO = OBSObserver()
-    assert OBSO.isconnected
