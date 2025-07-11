0d8f34997576a4a0cae1f9bc2066dc8daa70b30f
Justus <justus.d2025@gmail.com>
2025-07-11 23:57:42 +0200

    0.9.6

--- testing.py
+++ testing.py
@@ -14,16 +14,24 @@ def get_silence(filepath: str,silence: int = -36, duration: float = 0.5) -> list
 
             else:
                 data.append(float(args[2]))
-            #print(data[-1])
+
     l = len(data)
     return {i: (data[i] , data[i+1]) for i in range(0,l,2)}
         
     #! Will not see the last one when odd
+    
+def convert_to_tc(t:float):
+    h, m, s = t // 60 // 60,t // 60, t % 60
+
+    h = f'0{h}' if h < 10 else str(h)
+    m = f'0{m}' if m < 10 else str(m)
+    m = f'0{m}' if m < 10 else str(m)
+    return f'{h}:{m}:{s}'
 path = "C:\\Users\\Justus\\jri_data\\audio\\1_schedule_one_track_mic.aac"
 result = get_silence(path)
 print(result)
 for key in result:
-    
-    ffmpeg_run(FFPLAY_PLAY_AUDIO,{'__IN__': path,'__SS__': result['start'],'__TO__': result['end']})
+    print(convert_to_tc(result[key][0]),convert_to_tc(result[key][1]))
+    ffmpeg_run(FFPLAY_PLAY_AUDIO,{'__IN__': path,'__SS__': result[key][0],'__TO__': result[key][1]})
     #TODO SOX Command
 
