4cdcac350ed27a2242b38b85d1753744b6172676
Justus <justus.d2025@gmail.com>
2025-07-13 01:43:02 +0200

    0.9.34

--- bin/lprtplay.py
+++ bin/lprtplay.py
@@ -6,9 +6,7 @@ def play_audio(filepath: str):
     music.load(filepath)
     music.play()
 
-play_audio("C:\\Users\\Justus\\jri_data\\audio\\1_schedule_one_track_desktop.mp3")
-while music.get_busy():
-    print(music.get_pos())
+
 
 """
 Pseudo code:
