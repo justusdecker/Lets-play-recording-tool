3a4b454edff1e1b43fe7b33fa643cb8fc1a86ffa
Justus <justus.d2025@gmail.com>
2025-07-12 23:39:46 +0200

    0.9.32

--- bin/lprtplay.py
+++ bin/lprtplay.py
@@ -9,3 +9,18 @@ def play_audio(filepath: str):
 play_audio("C:\\Users\\Justus\\jri_data\\audio\\1_schedule_one_track_desktop.mp3")
 while music.get_busy():
     print(music.get_pos())
+
+"""
+Pseudo code:
+
+For noise in episode_noises:
+    play_audio noise
+    user input:
+        ok  -> set the noise_file -> next episode
+        not okay -> next audio noise -> Break Workflow if no audio existing anymore (later)
+At the end:
+
+For noise in okay_noise:
+    sox create noise_profile with noise
+    sox apply noise_profile
+"""
