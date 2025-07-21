0a90a7f759b5da925c306c2e8154c1292ae538d6
Justus <justus.d2025@gmail.com>
2025-07-21 12:43:15 +0200

    0.10.43

--- gui.py
+++ gui.py
@@ -137,7 +137,7 @@ def get_episode_range(parent, run_callback: callable, check_callback: callable,f
 
     This function sets up two labels ("Episode start", "Episode end"),
     two option menus for selecting start and end episode numbers, and an
-    "Extract" button. The button is initially enabled and its state
+    "Extract" button. The button is initially disabled(if ft is none <- No data exists) and its state
     can be managed by the `check_callback`. The `run_callback` is
     executed when the "Extract" button is clicked.
     """
