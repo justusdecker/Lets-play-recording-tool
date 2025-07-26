77981f94d9de6d8ddee0cf496d9d980c97a74c00
Justus <justus.d2025@gmail.com>
2025-07-26 11:25:43 +0200

    0.11.10

--- bin/jinjatest.py
+++ bin/jinjatest.py
@@ -1,13 +1,8 @@
 
-import jinja2
-def file_write(filepath : str, data : str):
-    with open(filepath, 'w') as f:
-        f.write(data)
-def file_read(filepath : str) -> str:
-    with open(filepath, 'r') as f:
-        return f.read()
-
-JINJA_ENVIRONMENT = jinja2.Template(file_read('static\\deploy.html'))
+from jinja2 import Template
+from bin.data_access import file_read,file_write
+def deploy_render(**replacers):
+    JINJA_ENVIRONMENT = Template(file_read('static\\deploy.html'))
 
 
-file_write('test.html',JINJA_ENVIRONMENT.render(title='TEST',episodes=[{'id':3},{'id':6},{'id':2},]))
+    file_write('test.html',JINJA_ENVIRONMENT.render(**replacers))
