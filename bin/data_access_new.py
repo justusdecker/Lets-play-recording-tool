e60c6a328d1e54b9751bf462da5231c63096b250
Justus <justus.d2025@gmail.com>
2025-07-31 16:48:16 +0200

    0.12.14

--- bin/data_access_new.py
+++ bin/data_access_new.py
@@ -86,8 +86,16 @@ class SQLAccess:
     def delete_letsplay(lpid:int):
         data = session.query(LetsPlays).all()[lpid]
         session.delete(data)
+        l = len(SQLAccess.read_episodes(lpid))
+        print(l)
+        for i in range(l):
+            SQLAccess.delete_episode(lpid,i)
         session.commit()
-        
+    
+    def delete_episode(lpid: int, epid: int):
+        data = session.query(Episodes).filter(SQLAccess.read_letsplays()[lpid].id == Episodes.lpid).all()[epid]
+        session.delete(data)
+        session.commit()
 
 ## Read Values
 #for episode in session.query(Episodes).all():
--- test_sql.py
+++ test_sql.py
@@ -1,14 +1,14 @@
 from bin.data_access_new import *
 from random import randint as ri
-#for i in range(3):
-#    SQLAccess.create_letsplay(f'Lets Play {i}','XXX',ri(1200,1800))
-#    r = ri(5,100)
-#    print(r)
-#    for j in range(r):
-#        SQLAccess.create_episode(i,f'{j}.mp4')
+for i in range(3):
+    SQLAccess.create_letsplay(f'Lets Play {i}','XXX',ri(1200,1800))
+    r = ri(5,100)
+    print(r)
+    for j in range(r):
+        SQLAccess.create_episode(i,f'{j}.mp4')
 SQLAccess.delete_letsplay(0)
-
-
+print(len(SQLAccess.read_episodes(0)))
+SQLAccess.delete_episode(0,1)
 print(len(SQLAccess.read_episodes(0)))
 print(len(SQLAccess.read_episodes(1)))
 print(len(SQLAccess.read_episodes(2)))
