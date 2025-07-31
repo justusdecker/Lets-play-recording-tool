15881f3de6d844522124dcce216ca32b85f9bfc5
Justus <justus.d2025@gmail.com>
2025-08-01 01:39:50 +0200

    0.12.31

--- bin/constants.py
+++ bin/constants.py
@@ -20,32 +20,15 @@ Some features might not work as expected & can cause data loss! Be careful!
 """
 
 DEFAULT_THUMBNAIL_SIZE = (1280,720)
+from enum import Enum
 
-LP_KEYS = [
-    'version',
-    'episode_path',
-    'tad_path',
-    'name',
-    'game_name',
-    'episode_length',
-    'description_path'] 
-
-EP_KEYS = [
-    'video_path',
-    'audio_mic_path',
-    'audio_desktop_path',
-    'thumbnail_path',
-    'thumbnail_frame',
-    'has_problem',
-    'audio_mic_edit1_path',
-    'audio_mic_edit2_path',
-    'audio_desktop_edit1_path',
-    'audio_desktop_edit2_path',
-    'title',
-    'episode_number',
-    'upload_at',
-    'final_audio'
-]
+FLAGS = Enum('Flags',
+             [
+                 'BLOCK_RECORDING',
+                 'BLOCK_AUTOMATION',
+                 'BLOCK_ALL'
+                 ]
+             )
 
 DEFAULT_OBS_SETTINGS = {
     "ip": "",
