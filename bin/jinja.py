f1d7ac246550a9a40278e9d4e06ee7a23a77bd49
Justus <justus.d2025@gmail.com>
2025-08-08 18:29:44 +0200

    0.16.4

--- bin/jinja.py
+++ bin/jinja.py
@@ -10,6 +10,4 @@ except:
 from bin.data_access import file_read,file_write
 def deploy_render(*args,**replacers):
     JINJA_ENVIRONMENT = Template(file_read('static\\deploy.html'))
-
-
     file_write(args[0],JINJA_ENVIRONMENT.render(**replacers))
