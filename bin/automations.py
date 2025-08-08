b169677500b3e12a560f8aef101c38508c324d45
Justus <justus.d2025@gmail.com>
2025-08-08 23:54:27 +0200

    0.16.23

--- bin/automations.py
+++ bin/automations.py
@@ -324,7 +324,7 @@ class SendToAudacityWF(GenericWorkFlow):
                 new = FIXED_AUDIO_FOLDER+f'{rng_list[ep]}_track_mic_fixed_ac.aac'
                 rie(new)
                 ffmpeg_run(FFMPEG_CONVERT_AUDIO_TYPE,{'__IN__': old, '__OUT__': new})
-                reoc(not isfile(new))
+                reoc(not isfile(new),ERROR_007)
                 #remove()
                 SQLAccess.update_episodes(self.lpid,rng_list[ep],audio_mic_edit2_path=new)
             app.start_btn.state(['!disabled'])
