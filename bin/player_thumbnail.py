bea68c7eb579815fd5c7f63fd3103944df621818
Justus <justus.d2025@gmail.com>
2025-08-09 03:58:14 +0200

    0.16.33

--- bin/player_thumbnail.py
+++ bin/player_thumbnail.py
@@ -12,6 +12,12 @@ except:
     quit()
 
 class ThumbnailPreview(Toplevel):
+    """
+    A Toplevel window for displaying a thumbnail preview.
+
+    This window shows an image, typically a thumbnail, updates its title
+    and displayed image based on the provided path and index.
+    """
     def __init__(self):
         super().__init__()
         self.isfinished = False
@@ -21,6 +27,17 @@ class ThumbnailPreview(Toplevel):
         self.update_image(f'bin\\data\\img\\logo.ico',-1)
 
     def update_image(self,path: str,i:int | None):
+        """
+        Updates the displayed image and the window title.
+
+        Resizes the image from the given path and displays it on the label.
+        The window title is updated to reflect the episode number or "TADEditor" in case of using this with the TADEditor.
+
+        Args:
+            path (str): The file path to the image to be displayed.
+            i (int | None): An optional index for the image, used in the window title.
+                            If None, "TADEditor" is used in the title.
+        """
         self.title(f'Thumbnail Preview: {i+1 if i is not None else "TADEditor"}')
         self.image = Image.open(path).resize((640,360))
         self.image = ImageTk.PhotoImage(self.image)
