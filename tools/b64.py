d6905c1a9a04579d3b04b996d4252463f739a012
Justus <justus.d2025@gmail.com>
2025-08-12 13:59:18 +0200

    0.17.2

--- tools/b64.py
+++ tools/b64.py
@@ -1,12 +1,9 @@
 """
 This converts images to ascii like strings
 """
-
 import base64
-from PIL import ImageTk, Image
-from io import BytesIO
 
-with open('bin\\data\\img\\ui\\run.png', 'rb') as f:
+with open('bin\\data\\img\\ui\\pause.png', 'rb') as f:
     print(len(f.read()))
     base64string = base64.b64encode(f.read()).decode('ascii')
     
@@ -14,16 +11,3 @@ with open('bin\\data\\img\\ui\\run.png', 'rb') as f:
     with open('tools\\img.txt','w') as fo:
         fo.write(base64string)
 print(base64string)
-
-
-from os import mkdir, remove
-
-from tkinter import PhotoImage
-
-class AsciiImage:
-    def __init__(self, var: str):
-        self.var = var
-
-        io = BytesIO(base64.b64decode(var.encode('ascii')))
-        img = Image.open(io)
-        self.image = ImageTk.PhotoImage(img)
