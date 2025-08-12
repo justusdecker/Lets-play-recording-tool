f5f74250a994bad98980184d6967bf486083dd6b
Justus <justus.d2025@gmail.com>
2025-08-12 14:22:06 +0200

    0.17.5

--- bin/welcome_popup.py
+++ bin/welcome_popup.py
@@ -1,7 +1,19 @@
 import pygame as pg
-from bin.constants import DISCLAIMER
+from bin.constants import DISCLAIMER, IMG_LOGO, IMG_WELCOME
 from threading import Thread
-from bin.data_access import AsciiImage
+import base64
+from PIL import ImageTk, Image
+from io import BytesIO
+class AsciiImage:
+    def __init__(self, var: str):
+        self.var = var
+
+        decoded_data =  base64.b64decode(var.encode('ascii'))
+        io_stream = BytesIO(decoded_data)
+        img = Image.open(io_stream)
+        self.image = pg.image.frombytes(
+        img.tobytes(), img.size, img.mode)
+
 class Welcome:
     """
     This is the welcome popup
@@ -11,13 +23,14 @@ class Welcome:
         self.window = pg.display.set_mode((800,400),pg.NOFRAME)
         pg.display.set_caption('Welcome to LPRT')
         self.isrunning = True
-        self.welcome = pg.image.load('bin\\data\\img\\welcome.png')
+        
+        self.welcome = AsciiImage(IMG_WELCOME).image
         
         self.clock = pg.time.Clock()
         pg.font.init()
         self.font = pg.font.Font(pg.font.get_default_font(),15)
         self.disclaimer = self.font.render(DISCLAIMER,True,(255,255,255))
-        self.logo = pg.transform.scale(pg.image.load('bin\\data\\img\\logo.png'),(self.disclaimer.get_height(),self.disclaimer.get_height()))
+        self.logo = pg.transform.scale(AsciiImage(IMG_LOGO).image,(self.disclaimer.get_height(),self.disclaimer.get_height()))
         pg.display.set_icon(self.logo)
         self.font.set_point_size(20)
         self.update_message('Here i am...')
