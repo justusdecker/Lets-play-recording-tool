51133c5267e0183048a58c2b45abaa7a420a5a1c
Justus <justus.d2025@gmail.com>
2025-08-17 01:48:26 +0200

    0.18.8

--- bin/download_file.py
+++ bin/download_file.py
@@ -1,8 +1,10 @@
-import requests
+from tkinter.messagebox import showerror
+from requests import get
+from requests.exceptions import HTTPError, RequestException
 import zipfile
 from io import BytesIO
 
-def download_file(url: str, filepath: str | None = None, mode: bool = False) -> BytesIO | None:
+def download_file(url: str, filepath: str | None = None, mode: bool = False) -> BytesIO | HTTPError | RequestException | bool:
     """
     Downloads a file from the Internet.
 
@@ -14,20 +16,27 @@ def download_file(url: str, filepath: str | None = None, mode: bool = False) -> 
             Returns BytesIO
             filepath can be all. It is never used anyway
     """
-    r = requests.get(url)
-    if mode:
-        with open(filepath, 'wb') as file:
-            file.write(r.content)
-    else:
-        return BytesIO(r.content)
-
-
+    try:
+        r = get(url)
+        r.raise_for_status()
+        if mode:
+            with open(filepath, 'wb') as file:
+                file.write(r.content)
+        else:
+            return BytesIO(r.content)
+    except (HTTPError, RequestException) as E:
+        return E
+        
+        
 def download_ffmpeg():
     """
     Downloads FFMPEG from gyan.dev
     """
     url = 'https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip'
     file = download_file(url)
+    if file is not None and file is not BytesIO:
+        showerror('Cannot download file', f'Statuscode: {file.response.status_code}')
+        return
     zip = zipfile.ZipFile(file)
     for ext in ['ffmpeg.exe','ffprobe.exe','ffplay.exe']:
         with open(ext,'wb') as f:
