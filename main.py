d3fe631d4e673f19c58029e4141371af467852b7
Justus <justus.d2025@gmail.com>
2025-08-21 11:39:49 +0200

    0.20.28

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ from bin.data_access import on_start
 from os import system
 from bin.download_file import send_heartbeat, get_newest_version_number
 from bin.ui import TkinterApp
-
+from bin.dll_loader import create_libpng16_16_ine
 def check_version():
     if get_newest_version_number()['version'] != '_'.join(VERSION.split('.')[0:2]):
         if msgbox.askyesno('New Update avaiable','Do you want to visit the update website?'):
             system('start https://github.com/justusdecker/Lets-play-recording-tool/releases')
 
 if __name__ == '__main__':
+    create_libpng16_16_ine()
     on_start()
     send_heartbeat()
     WELCOME.update_message('Create App')
