dfe63d16803e1fb45e4c28a35ca07f5f2361c9d2
Justus <justus.d2025@gmail.com>
2025-08-25 21:20:32 +0200

    1.0.32

--- bin/player_video.py
+++ bin/player_video.py
@@ -65,9 +65,10 @@ class NewVideoPlayer(NewMediaPlayer):
         
         
         img = AsciiImage(ICO_TAKE_THUMBNAIL)
-        self.take_thumbnail_btn = ttk.Button(self.bar,image=img.image,command=self.gen_thumbnail)
+        self.take_thumbnail_btn = ttk.Button(self.controls,image=img.image,command=self.gen_thumbnail)
         self.take_thumbnail_btn.pack(side=LEFT, padx=5)
         self.take_thumbnail_btn.image = img.image
+        
     def reset(self,data,lpid):
         change_states([self.stop_button,self.play_button, self.pause_button, self.next_button, self.last_button, self.take_thumbnail_btn, self.update_title_button],'disabled')
         self.stop_video()
