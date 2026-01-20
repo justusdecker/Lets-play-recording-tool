b74494880cf65c7f241f7ad9fefbc71193cf5468
Justus <justus.d2025@gmail.com>
2026-01-20 05:47:59 +0100

    cleaned up codebase & added LPEP

--- main.py
+++ main.py
@@ -1,10 +1,57 @@
-from bin.views.modules import *
-from bin.views.letsplays import letsplayListView, letsplaySingleView
+
+from kivy.app import App, Builder
+from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.screenmanager import Screen
+from kivy.uix.widget import Widget
+
+# Layouts
+
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+
+# Extended Layouts
+
+from kivy.uix.scrollview import ScrollView
+
+# Default UIX
+
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.image import Image
+from kivy.uix.popup import Popup
+
+from kivy.core.image import Image as CoreImage
+
+# Advanced UIX
+
+from kivy.uix.videoplayer import VideoPlayer
+
+# Portfunction-relevant imports
+
+import base64
+from io import BytesIO
+
+# Portfunctions from b64 -> Image
+
+def b64toki(encoded_data: str, extension: str = 'png'):
+    """
+    Converts Base64-Encoded Image-Strings to Kivy usable Images.
+    """
+    decoded_data =  base64.b64decode(encoded_data.encode('ascii'))
+    io_stream = BytesIO(decoded_data)
+    texture = CoreImage(io_stream, ext=extension).texture
+    return texture
+
+
+
 from kivy.properties import ObjectProperty
 
-from bin.template.menu_bar import MenuBar
 
 Builder.load_file('./application/main.kv')
+
+class LPEPSub(BoxLayout):
+    ...
 
 class MenuSub(BoxLayout):
     ...
