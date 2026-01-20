07b7a953380c74333c23b2405df3512a84b93955
Justus <justus.d2025@gmail.com>
2026-01-20 06:48:39 +0100

    cleaned up codebase again & moved data/

--- bin/api/dll_loader.py
+++
@@ -1,7 +0,0 @@
-from bin.api.dlls import LIBPNG16_16DLL
-from os.path import isfile
-def create_libpng16_16_ine():
-    """ creates libpng16-16.dll if not exist """
-    if not isfile('libpng16-16.dll'):
-        with open('libpng16-16.dll','wb') as fo:
-            fo.write(LIBPNG16_16DLL)
