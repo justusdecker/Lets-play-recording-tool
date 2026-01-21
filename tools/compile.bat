749451965f457310a4d2aa4abe226dda21db2957
Justus <justus.d2025@gmail.com>
2026-01-21 04:26:28 +0100

    removed old tools

--- tools/compile.bat
+++
@@ -1,6 +0,0 @@
-:: Takes the main.spec file & compiles the project into onefile
-:: Upside: Simple to use
-:: Downside: The Windows Defender nukes the zip! 
-pyinstaller main.spec
-python zipit.py
-pause 2>NUL
--- tools/libpng_parser.py
+++
@@ -1,7 +0,0 @@
-"""
-Move this file temporary into root & executing it will copy the `libpng16-16.dll` into `bin.dll_loader.py`
-"""
-with open('tools\\libpng16-16.dll','rb') as fi:
-    data = fi.read()
-with open('bin\\dll_loader.py','w') as fo:
-    fo.write(f'LIBPNG16_16DLL = {data}')
