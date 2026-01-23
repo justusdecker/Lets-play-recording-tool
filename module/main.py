d7824e9da81c6cc951723c8972819e6dc66211c5
Justus <justus.d2025@gmail.com>
2026-01-23 03:56:33 +0100

    obsws is now catching errors

--- module/main.py
+++ module/main.py
@@ -1,7 +1,8 @@
-from src.api.kivy_modules import BoxLayout
+from src.api.kivy_modules import BoxLayout, Button
 from src.api.obsws import OBSWSClient
 from threading import Thread
 from time import sleep
+from src.api.msgbox import msgbox, SoundFlags, MSGBoxPresets
 
 class OBSView(BoxLayout):
     is_watching: bool = False
@@ -21,15 +22,19 @@ class OBSView(BoxLayout):
             
             
     def __uri(self, ): 
-        obs_connection = OBSWSClient(password='')
+        obs_connection = OBSWSClient(password='KLj31bU8H2vrYQg6')
         if obs_connection.connect() is not None:
-            self.ids.timer.text = 'Cannot connected to OBS'
+            self.ids.timer.text = 'Connected to OBS'
         
         while self.is_watching:
             result = obs_connection.call("GetOutputStatus",{"outputName": 'adv_file_output'})
+            if 'error' in result:
+                msgbox('error', result['error'], MSGBoxPresets.SYSTEM_ALERT, SoundFlags.ERROR)
+                break
             try:
+                
                 self.ids.timer.text = result['d']['responseData']['outputTimecode']
-            except:
+            except Exception as E:
                 ...
             sleep(0.02)
         self.ids.timer.text = 'Disconnected'
@@ -44,6 +49,12 @@ class MenuSub(BoxLayout):
     
 class StartView(BoxLayout):
     ...
+    
+def overwrite(x: BoxLayout):
+    
+    x.clear_widgets()
+    for i in range(4):
+        x.add_widget(Button(text=f'{i}',size_hint_y = None, height = 40, on_press = lambda _instance: overwrite(x)))
     
 def start():
     # Your code here
