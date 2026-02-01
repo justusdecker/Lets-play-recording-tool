ddc0f4e9f14b83d6e764278a43dba531300ded1e
Justus <justus.d2025@gmail.com>
2026-02-01 20:26:00 +0100

    aio

--- insert_into_ext.bat
+++
@@ -1,5 +0,0 @@
-set dest=%USERPROFILE%\.vscode\extensions\lprtscript\
-mkdir %dest%
-mkdir %dest%\syntaxes\
-copy .\src\api\syntax\lprtscript.json %dest%\syntaxes\schema.tmLanguage.json
-copy .\src\api\syntax\package.json %dest%\package.json
