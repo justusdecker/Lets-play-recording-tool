bfe2f017441facc52268b2e43221108d03d5e253
Justus <justus.d2025@gmail.com>
2026-02-01 09:36:17 +0100

    moved back to tkinter

--- main.py
+++ main.py
@@ -1,50 +1,9 @@
-VERSION = '2.0.12'
 
+from src.api.tkinter_app import TkinterApp
 
-from src.api.kivy_modules import *
-
-from kivy.properties import ObjectProperty
-from src.api.module_loader import DatabaseLoader
-
-from typing import Callable
-
-Builder.load_file('./src/api/application/main.kv')
-
-import module.main as main_overwrite #! Try / Except this later: Building otherwise the build process will fail
-
-Builder.load_file('./module/extension.kv') #! Try / Except this later: Building otherwise the build process will fail
-main_overwrite.start()
-
-class LPEPSub(BoxLayout):
-    run_callback: Callable | None = ObjectProperty(None)
-
-
-
-class WelcomeView(FloatLayout):
-    """
-    The Startup View.
-    """
-    def start_app(self, *_):
-        print('This worked')
-        app.screen_manager.current = 'testView'
-    
-class DropDownSelector(BoxLayout):
-    update_entrys_callback: Callable | None = ObjectProperty(None)
-
-class MainWidget(FloatLayout):
-    manager = ObjectProperty(None)
-    ...
-
-class LPRTApp(App):
-    screen_manager = ObjectProperty(None)
-    def build(self):
-        root = MainWidget()
-        self.screen_manager = root.ids.screen_manager
-        
-        return root
 
 if __name__ == '__main__':
-
-    app = LPRTApp()
-    app.run()
-
+    APP = TkinterApp()
+    APP.mainloop()
+     
+    
