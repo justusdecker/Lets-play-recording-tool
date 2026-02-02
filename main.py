bea1613819a82f7391130e8215558629a8387e6e
Justus <justus.d2025@gmail.com>
2026-02-02 03:51:03 +0100

    added test views

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
-from src.api import TkinterApp
-
-
+from src.api import TkinterApp, AddView
+from module.module_main import Recording, FetchAudio
+AddView(Recording)
+AddView(FetchAudio)
 if __name__ == '__main__':
     APP = TkinterApp()
     APP.mainloop()
